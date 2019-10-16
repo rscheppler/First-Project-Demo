85251a1d1c516bc9d6c85d26810f61f7722002b2
kat <kathleen.hayes@digpen.edu>
2019-10-15 18:16:43 -0700

    had to make a project for no coder

---
+++ FlatPlatformer_nocode/Assets/Flat Platformer Template/Scripts/WindMill.cs
@@ -0,0 +1,23 @@
+﻿/* By: Bulat Sultanov
+ * Date: 2017
+ * Description: Rotates windmill over the update function
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMill : MonoBehaviour {
+    public float _Speed;
+    private float _currDist, _dist;
+
+    void Update()
+    {
+        if(Mathf.Round(_currDist) == Mathf.Round(_dist) || _currDist == 0)
+        {
+            _dist = Random.Range(0, 2.7f);
+        }
+        _currDist = Mathf.Lerp(_currDist, _dist, Time.deltaTime*0.3f);
+        transform.Rotate(0, 0, _Speed * Time.deltaTime + _currDist);
+    }
+}
